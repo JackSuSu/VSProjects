01e785c3b6352ee5a607b8f29421cd7523afd981
jacksusu <jacksu901124@gmail.com>
2015-04-02 15:15:52 +0800

    访问修饰符

--- 17访问修饰符/Program.cs
+++ 17访问修饰符/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _17访问修饰符02;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +7,42 @@ namespace _17访问修饰符
 {
     class Program
     {
+
+        /// <summary>
+        /// 
+        /// 已经解除的访问修饰符
+        /// 
+        /// public :公开的
+        /// 
+        /// private :私有的
+        /// 
+        /// protect：只能在自身或者子类中使用
+        /// 
+        /// internal:
+        /// 
+        /// 能修饰类的修饰符只有public 和 internal
+        /// 
+        /// Class 默认的Internal 当前程序集可以使用
+        /// 
+        /// 同一个程序集中：internal 比 projected 访问权限高
+        /// 
+        /// 不同程序集中：继承类的protected的修饰级别 可以传递跨程序集
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+
         static void Main(string[] args)
         {
-        }
+           
+
+            
+         }
     }
+
+
+
+
+
+
 }
---
+++ 17访问修饰符02/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17访问修饰符02
+{
+    internal class Person
+    {
+        protected string _name=string.Empty;
+
+    }
+
+    internal class Student : Person
+    {
+     
+    }
+}
