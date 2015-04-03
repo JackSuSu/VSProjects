76a9ce2d2aecbee4b1cd7d0d7d98329e9b8f572e
jacksusu <jacksu901124@gmail.com>
2015-04-03 15:33:17 +0800

    收银系统

---
+++ 24收银台/ProductFather.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _24收银台
+{
+    class ProductFather
+    {
+       
+
+        private double myVar;
+
+        public double Price
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+        private int _count;
+
+        public int Count
+        {
+            get { return _count; }
+            set { _count = value; }
+        }
+
+        private string  _id;
+
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+
+        public ProductFather(string id,double price,int count,string name)
+        {
+            this.ID=id;
+
+            this.Price=price;
+
+            this.Count=count;
+
+            this.Name = name;
+
+        }
+
+
+    }
+
+
+    class Sumngs:ProductFather
+    {
+        public Sumngs(string id, double price, int count,string name)
+            : base(id, price, count,name)
+        {
+ 
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// 宏基
+    /// 
+    /// </summary>
+
+    class Acer : ProductFather
+    {
+        public Acer(string id, double price, int count,string name)
+            : base(id, price, count,name)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// 香蕉
+    /// 
+    /// </summary>
+
+    class Banana : ProductFather
+    {
+        public Banana(string id, double price, int count, string name)
+            : base(id, price, count, name)
+        {
+
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// 酱油
+    /// 
+    /// </summary>
+
+    class JiangYou : ProductFather
+    {
+        public JiangYou(string id, double price, int count,string name)
+            : base(id, price, count, name)
+        {
+
+        }
+    }
+
+
+
+}
---
+++ 24收银台/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _24收银台
+{
+    class Program
+    {
+
+        /// <summary>
+        /// 
+        /// 基本模块面板
+        /// 
+        /// 1.仓库    2.产品    3.收银台
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+
+
+
+
+
+
+
+}
