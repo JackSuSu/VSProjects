ed96aa5ae0018c8ff887d7b3592bd56385c80284
jacksusu <jacksu901124@gmail.com>
2015-04-03 11:35:56 +0800

    接口

--- 21接口/Program.cs
+++ 21接口/Program.cs
@@ -12,7 +12,7 @@ namespace _21接口
         /// 
         ///     多态的另一种形式叫做接口
         /// 
-        /// 理解：
+        /// 理解：接口可以集成接口,接口显示实现可以避免接口和类的方法名相同的情况，类中默认的访问修饰符是 private 接口中默认的是public
         /// 
         /// 1.接口即使一种规范
         /// 
@@ -26,16 +26,36 @@ namespace _21接口
         /// 
         /// 问题：一个人类即想扣篮又想做学生
         /// 
-        /// 什么时候使用接口？
+        /// 什么时候使用接口？(面向接口的编程，手机APP 天机APP，数据从哪里？中央气象台，从中央气象实现接口，就可以拿到信息，就可以拿到信息)
+        /// 
+        /// （支付宝接口，图书商场淘宝，实现接口就可以实现里面的功能）
         /// 
         /// 1.多继承的时候 即想这样又想那样
+        /// 
+        /// 2.
         /// 
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
 
+            //ICouLanable coulan = new NBAPlayer();
 
+            //coulan.KouLan();
+
+            IFlyable ip = new Person();
+
+            ip.Fly();
+
+            IFlyable ib = new Bird();
+
+            ib.Fly();
+
+            IFlyable iss = new Student();
+
+            iss.Fly();
+
+            Console.ReadKey();
          
         }
     }
@@ -49,7 +69,7 @@ namespace _21接口
     /// 
     /// </summary>
 
-    public class Person
+    public class Person:IFlyable
     {
 
 
@@ -87,24 +107,65 @@ namespace _21接口
         {
             Console.WriteLine("吃喝拉撒睡！");
         }
+
+        public void Fly()
+        {
+
+            Console.WriteLine("人类在飞！");
+           // throw new NotImplementedException();
+        }
+
+        string IFlyable.name
+        {
+            get;
+            set;
+        }
     }
 
-    public class NBAPlayer : Person
+    public class NBAPlayer : Person,ICouLanable,IFlyable
     {
-        public void couLan()
+     
+
+        public void KouLan()
         {
             Console.WriteLine("我会扣篮！");
+        }
+
+        void IFlyable.Fly()
+        {
+
+            Console.WriteLine("学生在飞！");
+
+           // throw new NotImplementedException();
+        }
+        string IFlyable.name
+        {
+            get;
+            set;
         }
     }
 
 
 
-    public class Student : Person,ICouLanable
+    public class Student : Person, ICouLanable, IFlyable
     {
 
         public void KouLan()
         {
             Console.WriteLine("我可以扣篮！");
+        }
+
+        public void Fly()
+        {
+
+            Console.WriteLine("学生在飞！");
+          //  throw new NotImplementedException();
+        }
+
+        public string name
+        {
+            get;
+            set;
         }
     }
 
@@ -125,6 +186,8 @@ namespace _21接口
     /// 
     /// 接口的定义
     /// 
+    /// 接口不能被实例化（静态类，接口，抽象类）
+    /// 
     /// </summary>
 
     public interface IFlyable
@@ -143,7 +206,7 @@ namespace _21接口
 
         //不能写字段
 
-        //可以写属性
+        //可以写属性(自动属性)
 
         string name
         {
@@ -152,5 +215,71 @@ namespace _21接口
         }
 
     }
+
+
+    public class Bird : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("鸟在飞！");
+          //  throw new NotImplementedException();
+        }
+
+        public string name
+        {
+            get;
+            set;
+        }
+    }
+
+
+
+    public interface M1
+    {
+        void Test1();
+    }
+
+    public interface M2
+    {
+        void Test2();
+    }
+
+    public interface M3
+    {
+        void Test3();
+    }
+
+
+    public interface SupperInterface : M1, M2, M3
+    {
+ 
+    }
+
+
+    public class TestClass : SupperInterface
+    {
+
+        public void Test1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Test2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Test3()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Test2Class : TestClass 
+    {
+
+    }
+
+
 
 }
---
+++ 22接口联系/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22接口联系
+{
+    class Program
+    {
+
+        /// <summary>
+        /// 
+        /// 麻雀会飞，鹦鹉会飞，鸵鸟不会飞，企鹅会飞，直升飞机会飞（有飞的能力）
+        /// 
+        /// 用多态实现
+        /// 
+        /// 需要方法，抽象类，接口
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+        }
+
+
+    }
+
+
+    public class Bird
+    {
+        public string wings
+        {
+            get;
+            set;
+        }
+        public void EatDrink()
+        {
+            Console.WriteLine("吃喝拉撒");
+        }
+    }
+
+    public class MaQue : Bird,IFlyable
+    {
+
+        public void Fly()
+        {
+            Console.WriteLine("我麻雀我会飞");          
+        }
+    }
+
+    public class YingWu : Bird, IFlyable,ISpeak
+    {
+
+        public void Fly()
+        {
+            Console.WriteLine("鹦鹉会飞");
+            //throw new NotImplementedException();
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("鹦鹉会说话");
+            //throw new NotImplementedException();
+        }
+    }
+
+    public class TuoNiao : Bird
+    {
+ 
+    }
+
+    public class QiE : Bird
+    {
+ 
+    }
+
+
+
+    /// <summary>
+    /// 
+    /// 接口能力1.IFly,ISpeak
+    /// 
+    /// </summary>
+
+    public interface IFlyable
+    {
+        void Fly();
+ 
+    }
+
+
+    public interface ISpeak
+    {
+        void Speak();
+    }
+
+}
