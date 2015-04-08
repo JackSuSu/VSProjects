9663c3a68876a595b8bff4a04c4b5981c34f1419
jacksusu <jacksu901124@gmail.com>
2015-04-08 13:31:22 +0800

    Socket

---
+++ 13Socket网络编程/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace _13Socket网络编程
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(622, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "开始监听";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(401, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "端口号";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "IP";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(457, 100);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(139, 21);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.Text = "8000";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(85, 168);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(612, 344);
+            this.txtLog.TabIndex = 5;
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(120, 100);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(268, 21);
+            this.txtIp.TabIndex = 6;
+            this.txtIp.Text = "192.168.102.222";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 610);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtIp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtIp;
+
+    }
+}
+
---
+++ 13Socket网络编程/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _13Socket网络编程
+{
+    /// <summary>
+    /// 
+    /// Socket 英文孔或者插座的意思
+    /// 
+    /// 计算机中进程的通信机制，套接字描述IP地址和端口号
+    /// 
+    /// 服务器（各种应用程序进程，比如数据库远程连接）
+    /// 
+    /// 客户端 -->连接服务器
+    /// 
+    /// 现在服务器-->客户端 通信 那么就需要知道服务器的IP地址，同时还要知道服务器服务的端口号：比如IIS 网站端口号
+    /// 
+    /// 过程：服务器进程开启：IP+端口号
+    /// 
+    /// 然后应用程序进行 三次握手连接认证，最后进行通讯。
+    /// 
+    /// 服务器和客户端进行交换数据
+    /// 
+    /// 服务器：Ip地址，客户端，某一个应用地址，应用程序所占用的端口号
+    /// 
+    /// 客户端：准确的连接到应用程序；
+    /// 
+    /// 协议：TCP/UDP协议，TCP比较安全稳定，不会丢失数据
+    /// 
+    /// TCP：三次握手 TCP要求必须有服务器（三次握手）
+    /// 
+    /// 1.你有空吗？     
+    /// 
+    /// 2.我有空！
+    /// 
+    /// 3.我知道你有空了
+    /// 
+    /// TCP好处：安全稳定，效率低 三次握手占用，时间慢
+    /// 
+    /// UDP：快速效率高，不稳定，容易发生 数据丢失
+    /// 
+    /// 客户端发消息：不管服务器忙不忙，全部发送，消息不验证，不管服务器忙不忙，直接数据包发送过去，单向的直传，服务器没时间处理，就出现数据丢失
+    /// 
+    /// 两个各有优缺点
+    /// 
+    /// 
+    /// Socket通信的基本流程
+    /// 
+    /// 服务器：
+    /// 
+    /// 1.创建Socket监听 并绑定了端口号
+    /// 
+    /// 2.设置监听队列 Listen(10) 默认监听队列（服务器在一个时间点内所能接纳的客户端是有限的）
+    /// 
+    /// 3.等待客户端的连接
+    /// 
+    /// 
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //开始监听的时候，在服务器创建一个负责监听的IP地址跟端口号的Socket
+
+
+            Socket socketwath = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+     
+            IPAddress ip = IPAddress.Parse(txtIp.Text);
+
+            //创建端口对象
+
+            IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+
+            socketwath.Bind(point);
+
+            //一个时间段内 只能接纳10个客户端
+
+            //厕所蹲坑 坑位+加便池=总容量 但是我设置厕所只有五个人能蹲坑
+            
+            //第十一个人来了排队
+
+            //Socket监听队列
+
+            ShowMsg("监听成功！");
+
+            socketwath.Listen(10);
+
+            Thread thread = new Thread(Listent);
+
+            thread.IsBackground = true;
+
+            thread.Start(socketwath);
+         
+
+            //IPEndPoint ip = IPAddress.Any;
+        }
+
+        /// <summary>
+        /// 
+        /// 等待客户端连接
+        /// 
+        /// 并创建与之通信的Socket
+        /// 
+        /// </summary>
+
+        void Listent(object sw)
+        {
+
+
+            Socket socketwath = (Socket)sw;
+
+
+            //Accept 等待客户端连接
+
+            //Accept 来接受客户端的请求 并为之创建一个socket
+
+            //返回的Socket 是负责通信的Socket
+
+
+            //服务器分配一个Socket给 客户端
+
+            //Accept 等待连接 一直持续性的连接 这个时候开启一个新的线程
+
+            //只能一个客户端 写在一个循环里面
+
+            while(true)
+            {
+
+
+                try
+                {
+
+                    //服务器分配一个Socket给 客户端
+
+                    Socket socksend = socketwath.Accept();
+
+                    //socketwath.
+
+                    //拿到远程服务器的IP和端口号
+
+                    ShowMsg(socksend.RemoteEndPoint.ToString() + ":" + "连接成功！");
+
+                    //客户端连接成功后 服务器应该接受客户端发来的消息
+
+                    Thread thread = new Thread(ReciveData);
+
+                    thread.IsBackground = true;
+
+                    thread.Start(socksend);
+
+
+                }
+                catch
+                {
+ 
+                }
+
+              
+
+            
+     
+
+            }
+
+          
+ 
+        }
+
+
+        /// <summary>
+        /// 
+        /// 关闭后 死循环
+        /// 
+        /// 接收不到数据后 跳出循环
+        /// 
+        /// </summary>
+        /// <param name="sw"></param>
+
+        private void ReciveData(object sw)
+        {
+
+
+            Socket socksend = (Socket)sw;
+
+            byte[] bufffer = new byte[1024 * 1024 * 2];
+
+            while(true)
+            {
+
+                try
+                {
+
+                    int realByte = socksend.Receive(bufffer);
+
+                    if (realByte == 0)
+                    {
+                        break;
+                    }
+
+                    string gedata = Encoding.UTF8.GetString(bufffer, 0, realByte);
+
+                    ShowMsg(socksend.RemoteEndPoint.ToString() + ":" + gedata);
+
+                }
+                catch
+                {
+ 
+                }
+
+            
+            }
+
+            
+        }
+
+
+        private void ShowMsg(string str)
+        {
+            txtLog.AppendText(str + "\r\n");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+    }
+}
